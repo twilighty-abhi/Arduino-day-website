3ee10a8ff6c9436e0a8ba1666bfd7dadbd1c52d7
Abhiram N J <abhiramnj@gmail.com>
2026-03-24 18:15:26 +0530

    feat: Add event registration and competition form URLs for Arduino Day 2026

---
+++ lib/event.ts
@@ -0,0 +1,8 @@
+/** Main Arduino Day event registration (TinkerHub). */
+export const EVENT_REGISTRATION_URL =
+  "https://tinkerhub.org/events/G5DBKWNQ9K/Arduino%20Day%202026";
+
+/** Line following competition (Google Form). Set to `false` if slots open again. */
+export const LINE_FOLLOWING_REGISTRATION_FULL = true;
+
+export const LINE_FOLLOWING_FORM_URL = "https://forms.gle/VAmtAYbFiyqGQM9m8";
